583a3ec07c36f622419f1fa5ccc14686bbde5cb3
Abdi <abdibekele2021@gmail.com>
2025-09-16 06:27:44 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ab_ecommerce_admin_panel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ab_ecommerce_admin_panel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:get/get.dart';
+
+
+Future<void> main() async {
+  /// Ensure that widgets are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Initialize Getx local storage
+   
+  /// Remove # sign from url
+   
+  /// Initialize Firebase and Authentication Repositories
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()))
+   
+  /// Main starts here
+  runApp(const App());
+}
